4a01f7d48fb6338efac7563e8fc6ace369902ece
Ranjan Kumar <85241554+Ranjanku@users.noreply.github.com>
2024-05-16 23:58:15 +0000

    added dataTypes and variables

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer versioin
+
+// alert(3+3) // error because this is node not browser.
+
+// code readablilty should be taken in care.
+
+let name = "Ranjan"
+let age = 20
+let isLoggedIn = false
+
+// number -> 2 to power 53.
+// big-int
+// string -> ``, " ". ' '
+// boolean -> true/false
+// null -> standalone value
+// undefined -> not assgined
+// symbol (VVI) -> unique
+// object
+
+
+// null is object.
+
+console.log(typeof "Hello")
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 12212348
+let accountEmail = "Ranjan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState; // undefined
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+var -> try do not to use var because, there is problem of scope.
+*/
