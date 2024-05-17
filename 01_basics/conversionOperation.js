dff2bacbb68fc0ec7f9567a60672264beedcfe18
Ranjan Kumar <85241554+Ranjanku@users.noreply.github.com>
2024-05-17 00:08:16 +0000

    learned type conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+console.log(typeof(score))
+
+let valueInNUmber = Number(score)
+console.log(typeof valueInNUmber)
+console.log(valueInNUmber)
+
+// "33" -> 33
+// "33ads" -> NaN
+// true -> 1, false -> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
